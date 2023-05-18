24849018821b9fc9e0069b8b3aea99a5c8c62d7e
Ryan <ryanstedmansmith@gmail.com>
2023-05-18 15:48:04 -0400

    finished navbar but can't figure out svg for logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
-import { useState } from 'react';
-import { Navbar, Container } from "react-bootstrap";
-import logo from '../assets/img/rsm-logo.svg';
+import { useState, useEffect } from 'react';
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
+// import { HashLink } from 'react-router-hash-link';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState ('home');
@@ -11,7 +15,7 @@ export const NavBar = () => {
             if (window.scrollY > 50) {
                 setScrolled(true);
             } else {
-                setScrolled (False);
+                setScrolled (false);
             }
         }
 
@@ -40,13 +44,13 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
+              {/* <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
+              </HashLink> */}
             </span>
           </Navbar.Collapse>
         </Container>
